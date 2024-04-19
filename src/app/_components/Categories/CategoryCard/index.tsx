7cc6046cdffce9342666f4edb025497fe9d07fd9
fahadkabali <kabalifahad@gmail.com>
2024-04-19 04:19:31 +0300

    ecommerce

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 
 import { Category } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
+import type {Media} from '../../../../payload/payload-types'
 
 import classes from './index.module.scss'
 
@@ -12,14 +13,14 @@ type CategoryCardProps = {
 }
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
-  // const media = category.media as Media
+  const media = category.media as Media
   const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href="/products"
       className={classes.card}
-      // style={{ backgroundImage: `url(${media.url})` }}
+      style={{ backgroundImage: `url(${media.url})` }}
       onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
